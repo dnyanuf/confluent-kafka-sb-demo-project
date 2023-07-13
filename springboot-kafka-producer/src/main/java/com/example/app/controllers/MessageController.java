01e637267473d5d93c3f9b81d37818c91688c888
fdany <fdnyanu@gmail.com>
2023-07-13 17:04:15 +0530

    master commit

---
+++ springboot-kafka-producer/src/main/java/com/example/app/controllers/MessageController.java
@@ -0,0 +1,27 @@
+package com.example.app.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.app.kafka.KafkaProducer;
+import com.example.app.payload.User;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class MessageController {
+
+    private KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        kafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to kafka topic");
+    }
+}
